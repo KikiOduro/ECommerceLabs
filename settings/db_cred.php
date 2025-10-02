4fd400b234c234376641cc6661346825fedc188e
KikiOduro <akua.oduro@ashesi.edu.gh>
2025-10-02 12:11:00 +0000

    update

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,6 +1,6 @@
 <?php
 
 if (!defined('SERVER'))   define('SERVER',   '127.0.0.1'); 
-if (!defined('USERNAME')) define('USERNAME', 'root');
-if (!defined('PASSWD'))   define('PASSWD',   'root');
-if (!defined('DATABASE')) define('DATABASE', 'dbforlab');
+if (!defined('USERNAME')) define('USERNAME', 'akua.oduro');
+if (!defined('PASSWD'))   define('PASSWD',   'Kiki@2025Pass!');
+if (!defined('DATABASE')) define('DATABASE', 'ecommerce_2025A_akua_oduro');
