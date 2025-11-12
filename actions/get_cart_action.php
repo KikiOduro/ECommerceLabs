32c0de8c811a1fd47a54519e505bf4e88cf75b66
KikiOduro <akua.oduro@ashesi.edu.gh>
2025-11-12 20:25:50 +0000

    update

--- actions/get_cart_action.php
+++ actions/get_cart_action.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json');
 require_once __DIR__ . '/../settings/core.php';
 require_once __DIR__ . '/../controllers/cart_controller.php';
