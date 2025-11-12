09886c0fab66a0008ecf097c8e02d876b973fb8a
KikiOduro <akua.oduro@ashesi.edu.gh>
2025-11-12 21:28:30 +0000

    new

--- admin/checkout.php
+++ admin/checkout.php
@@ -2,7 +2,7 @@
 if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
-require_once '../settings/core.php';
+require_once __DIR__ . '../settings/core.php';
 if (!isLoggedIn()) {
     header('Location: login/login.php');
     exit;
